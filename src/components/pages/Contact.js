23d473155ac3a54d90afbd7ae0de3013dbadc6d9
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2023-04-10 22:58:23 -0700

    added color to error message

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -31,21 +31,21 @@ const Contact = () => {
   <label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
   <input type="text" onChange={(e) => setFullName(e.target.value)} value={fullName} className="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
   {err && !fullName &&
-      <p>You must include full name</p>
+      <p>* You must include full name</p>
       }
 </div>
       <div className="mb-3">
   <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
   <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className="form-control" id="exampleFormControlInput2" placeholder="name@example.com" />
   {err && !email &&
-      <p>You must include an Email</p>
+      <p>* You must include an Email</p>
       }
 </div>
 <div className="mb-3">
   <label htmlFor="exampleFormControlTextarea1" className="form-label">Text Area</label>
   <textarea className="form-control" onChange={(e) => setTextArea(e.target.value)} value={textArea} id="exampleFormControlTextarea1" rows="3"></textarea>
   {err && !textArea &&
-      <p>You must include a descriptive message</p>
+      <p>* You must include a descriptive message</p>
       }
 </div>
       <Button type="submit">Submit</Button>
