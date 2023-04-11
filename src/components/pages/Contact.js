4aba4b7132b0f44c3dfedb235b6cb9ca87b1f325
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2023-04-10 21:41:16 -0700

    added error messages for contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,32 +1,63 @@
-import React from "react";
+import React, { useState }  from "react";
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+
 const Contact = () => {
+  
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [textArea, setTextArea] = useState('')
+
+  const [err, setErr] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!fullName || !email || !textArea) {
+      setErr(true)
+    }
+  } 
+
   return (
     <div>
       
       <h2> Contact Me </h2>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
       <div className="mb-3">
   <label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
+  <input type="text" onChange={(e) => setFullName(e.target.value)} value={fullName} className="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
+  {err && !fullName &&
+      <p>You must include full name</p>
+      }
 </div>
       <div className="mb-3">
   <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="name@example.com" />
+  <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className="form-control" id="exampleFormControlInput2" placeholder="name@example.com" />
+  {err && !email &&
+      <p>You must include an Email</p>
+      }
 </div>
 <div className="mb-3">
-  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">Text Area</label>
+  <textarea className="form-control" onChange={(e) => setTextArea(e.target.value)} value={textArea} id="exampleFormControlTextarea1" rows="3"></textarea>
+  {err && !textArea &&
+      <p>You must include a descriptive message</p>
+      }
 </div>
+      <Button type="submit">Submit</Button>
+      
+     
       </Form>
     
-      <Button>Submit</Button>
     
     </div>
   );
 };
+
+
 
 export default Contact;
